3d84d4f3e3ca8f9dca47ffe106dd14bb7792f250
shubham-patil99 <shubham.patil9964123@gmail.com>
2023-11-09 12:02:02 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Indian Marketing Company
         </p>
-        <a
+        <p
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          My New Marketing Company
+        </p>
       </header>
     </div>
   );
